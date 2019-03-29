551b6c4b602a734d61f7d165a7f5a9e73d27fde2
David Rojas <deznorth@gmail.com>
2019-03-29 03:04:50 -0400

    fixed header buttons crashing the site on blogpage

--- src/components/containers/Header/Header.jsx
+++ src/components/containers/Header/Header.jsx
@@ -2,7 +2,7 @@
  * @Author: David M. Rojas Gonzalez // davidr.info 
  * @Date: 2019-03-04 19:12:01 
  * @Last Modified by: David M. Rojas Gonzalez // davidr.info
- * @Last Modified time: 2019-03-28 14:51:34
+ * @Last Modified time: 2019-03-29 02:50:45
 */
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -11,6 +11,11 @@ import './Header.scss';
 import ResumeBtn from '../../elements/ResumeBtn/ResumeBtn';
 
 const Header = () => {
+
+    /* 
+        Remember to implement some way for the navBar to change depending on the current page
+    */
+
     return (
         <header className="App-header">
             <span className="app-brand">
@@ -23,13 +28,19 @@ const Header = () => {
             <span className="app-nav">
                 <ul>
                 <button onClick={()=>{
-                    document.getElementById("aboutSection-default").scrollIntoView({ behavior: 'smooth' });
+                    if(document.getElementById("aboutSection-default")){
+                        document.getElementById("aboutSection-default").scrollIntoView({ behavior: 'smooth' });
+                    }
                 }} className="header-link">about</button>
                 <button onClick={()=>{
-                    document.getElementById("SkillsSection").scrollIntoView({ behavior: 'smooth' });
+                    if(document.getElementById("SkillsSection")){
+                        document.getElementById("SkillsSection").scrollIntoView({ behavior: 'smooth' });
+                    }
                 }} className="header-link">skills</button>
                 <button onClick={()=>{
-                    document.getElementById("ProjectsSection").scrollIntoView({ behavior: 'smooth' });
+                    if(document.getElementById("ProjectsSection")){
+                        document.getElementById("ProjectsSection").scrollIntoView({ behavior: 'smooth' });
+                    }
                 }} className="header-link">projects</button>
                 <Link className="header-link" to="/blog" >Blog</Link>
                 </ul>
